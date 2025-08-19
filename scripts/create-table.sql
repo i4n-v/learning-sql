086e7d62fe38ed8c64500c7e85fef7cd0dc9ed7a
i4n-v <ianvinicioss@gmail.com>
2025-08-18 22:24:59 -0300

    feat: using PL/PgSQL to insert random register in a multiple tables

--- scripts/create-table.sql
+++ scripts/create-table.sql
@@ -9,5 +9,14 @@ CREATE TABLE IF NOT EXISTS customers (
   id SERIAL,
   first_name VARCHAR(20),
   last_name VARCHAR(60),
+  email VARCHAR(256), -- Is added on alter-table.sql
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE IF NOT EXISTS orders (
+  id SERIAL,
+  customer_id INT,
+  amount NUMERIC(7, 2)
 );
--- scripts/insert.sql
+++ scripts/insert.sql
@@ -54,3 +54,29 @@ VALUES (
   'rafael@email.com'
 )
 RETURNING id, created_at;
+
+
+-- A loop to insert multiple register on PostgreSql using PL/pgSQL
+
+DO $$
+DECLARE
+  i INT := 1;
+  m INT := 20;
+  customer_id INT;
+  created_customers INT := 0;
+
+BEGIN
+  WHILE i <= m LOOP
+    INSERT INTO customers(first_name, last_name, email)
+    VALUES('Customer-' || i, 'Doe-' || i, 'customer.' || i || '@email.com')
+    RETURNING id INTO customer_id;
+
+    INSERT INTO orders(amount, customer_id)
+    VALUES(RANDOM() * 1000, customer_id);
+
+    i := i + 1;
+    created_customers := created_customers + 1;
+  END LOOP;
+
+  RAISE NOTICE '% customers succesfully created', created_customers;
+END $$;
